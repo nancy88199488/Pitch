d72bf97f943ba0d26f70f65be19ee360cad2859a
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-14 10:21:08 +0300

    modifyig the  manage file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 
 # Initializing application
-app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
+
+ # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
--- manage.py
+++ manage.py
@@ -1,2 +1,15 @@
 from app import create_app, db
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import  Manager, Server
 from app.models import User
+from  flask_migrate import Migrate, 
+
+# Creating app instance
+app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+migrate = Migrate(app,db)
+
+MigrateCommandmigrate = Migrate(app, db)
+manager.add_command('db',MigrateCommand)
