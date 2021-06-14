356a456fd9140a2135832305b76380d00aba1d5a
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-14 10:42:51 +0300

    update the requirements file

--- manage.py
+++ manage.py
@@ -2,14 +2,27 @@ from app import create_app, db
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import  Manager, Server
 from app.models import User
-from  flask_migrate import Migrate, 
+from  flask_migrate import Migrate
 
 # Creating app instance
-app = create_app('production')
+app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
-migrate = Migrate(app,db)
 
-MigrateCommandmigrate = Migrate(app, db)
+
+migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__ == "__main__":
+    manager.run()
